16c4d8ea80ea57ab8a4f08efbdc7354726f0d0a5
guywhataguy <33753158+guywhataguy@users.noreply.github.com>
2020-05-30 04:22:56 +0300

    x table printing functionallity and smarter number generation

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # SimpleMathTraining
+
 Train your arthmatic skills by solving simple math exercises. I counldn't find what I was lookig for online so I decided to make this.
+
+## Road Map
+
+code will be iteratively improved with more features and more complex questions.
+Will start with simple addition
+
+### Todo
+
+* Set options as CLI args
+* Add *, /, - operators
+* Add ^ operator with own high->low limit
+* Work on complex expressions (chained simple expresssions)
+* Put corrected answer on wrong Qs in other color
+* Add logic to reask wrongly answered Qs
--- train.go
+++ train.go
@@ -41,18 +41,27 @@ func add(n1, n2 int) int {
 	return n1 + n2
 }
 
+func multiply(n1, n2 int) int {
+	return n1 * n2
+}
+
 var addition = basicOp{solver: add, sign: "+"}
+var multiplication = basicOp{solver: multiply, sign: "x"}
 
 // --- logic ---
+
+func randSelect(set []interface{}) interface{} {
+	return set[rand.Intn(len(set))]
+}
 
 func randRange(low, high int) int {
 	return rand.Intn(high-low) + low
 }
 
-func generateExpression(low, high int, ops []basicOp) basicExpr {
+func generateExpression(genNum1, genNum2 func() int, ops []basicOp) basicExpr {
 	return basicExpr{
-		num1: randRange(low, high),
-		num2: randRange(low, high),
+		num1: genNum1(),
+		num2: genNum2(),
 		op:   ops[randRange(0, len(ops))],
 	}
 }
@@ -121,7 +130,6 @@ func trainBasicExpr(exgen func() basicExpr, count int, slowThresh int) {
 
 	for _, resp := range responses {
 		if !resp.isCorrect {
-			fmt.Println("adding to wrong")
 			wrong = append(wrong, resp)
 		}
 		if resp.answerTimeMili > slowThresh {
@@ -133,7 +141,11 @@ func trainBasicExpr(exgen func() basicExpr, count int, slowThresh int) {
 	correctCnt := count - len(wrong)
 	colorstring.Fprintln(terminalANSI, "[cyan]Stats:")
 	if correctCnt == count {
-		colorstring.Fprintln(terminalANSI, "\n[cyan]PERFECT SCORE!\n")
+		colorstring.Fprintln(terminalANSI, "[cyan]PERFECT SCORE!")
+		colorstring.Fprintln(terminalANSI, "[cyan]PERFECT SCORE!")
+		colorstring.Fprintln(terminalANSI, "[cyan]PERFECT SCORE!")
+		colorstring.Fprintln(terminalANSI, "[cyan]PERFECT SCORE!")
+		colorstring.Fprintln(terminalANSI, "[cyan]PERFECT SCORE!")
 	}
 	colorstring.Fprintln(terminalANSI, fmt.Sprintf(" [green]Correct: %d/%d (%0.2f%%)", correctCnt, count, float64(correctCnt)/float64(count)*100))
 	colorstring.Fprintln(terminalANSI, fmt.Sprintf(" [yellow]Slow: %d/%d (%0.2f%%)", len(slow), count, float64(len(slow))/float64(count)*100))
@@ -162,22 +174,68 @@ func terminalClear() {
 	fmt.Print("\r")
 }
 
+func printMultiplicationTable(min, max, colorFrom int) {
+	clrprnt := func(s string) {
+		colorstring.Fprint(terminalANSI, s)
+	}
+	// make border
+	prefix := "[cyan]  *"
+	clrprnt(prefix)
+	for j := min; j < max; j++ {
+		clrprnt(fmt.Sprintf("[magenta]%5d", j))
+	}
+	fmt.Println()
+
+	var color string
+	for i := min; i < max; i++ {
+		clrprnt(fmt.Sprintf("[magenta] %2d", i))
+		for j := min; j < max; j++ {
+			if i > colorFrom || j > colorFrom {
+				color = "[green]"
+			} else {
+				color = ""
+			}
+			clrprnt(fmt.Sprintf("%s%5d", color, j*i))
+		}
+		fmt.Println()
+	}
+}
+
 var terminalANSI io.Writer
 
 func main() {
 	// initing
-
-	supportedOperations := []basicOp{addition}
-	selectedOperations := supportedOperations[:]
+	rand.Seed(time.Now().UTC().UnixNano())
+	supportedOperations := [...]basicOp{addition, multiplication}
+	selectedOperations := supportedOperations[1:]
+	slowThresh := 2 * 1000 // slow answer if > 3 secodns
+	exCount := 100
+	// MAX NUMS are not including
+	n1MinNum := 3
+	n1MaxNum := 12
+	n2MinNum := 6
+	n2MaxNum := 12
 	fmt.Println("Welcome")
 	fmt.Println("Current supported operations:", supportedOperations)
 	fmt.Println("Currently selected operations:", selectedOperations)
-	slowThresh := 3 * 1000 // slow answer if > 3 secodns
+	fmt.Println("Slow Threshold:", slowThresh, "ms")
+	fmt.Println("num1 range:", n1MinNum, "-", n1MaxNum)
+	fmt.Println("num2 range:", n2MinNum, "-", n2MaxNum)
 
 	terminalANSI = ansi.NewAnsiStdout()
+	if false {
+		printMultiplicationTable(4, 15, 10)
+		return
+	}
+	genNum1 := func() int {
+		return randRange(n1MinNum, n1MaxNum)
+	}
+	genNum2 := func() int {
+		return randRange(n2MinNum, n2MaxNum)
+	}
 
 	exerciseGenerator := func() basicExpr {
-		return generateExpression(0, 10, selectedOperations)
+		return generateExpression(genNum1, genNum2, selectedOperations)
 	}
-	trainBasicExpr(exerciseGenerator, 3, slowThresh)
+	trainBasicExpr(exerciseGenerator, exCount, slowThresh)
 }
